c00d9b54b807a6c0501a9c1fba77224e38eda865
rr1me <s1337228@yandex.ru>
2025-05-18 20:58:08 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите массив в формате [1, 0, 1, 1, ...]: ");
+        String input = scanner.nextLine().trim();
+
+        String cleanedInput = input.replaceAll("[\\[\\] ]", "");
+        String[] elements = cleanedInput.split(",");
+
+        int employeeCount = 0;
+        for (String element : elements) {
+            if (element.equals("1")) {
+                employeeCount++;
+            }
+        }
+
+        System.out.println("Количество сотрудников: " + employeeCount);
+    }
+}
