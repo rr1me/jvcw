8f12082fa87e98972b8b28f2e718df90bcbb34b5
rr1me <s1337228@yandex.ru>
2025-05-18 21:32:12 +0300

    cmting

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Введите массив в формате [1, 0, 1, 1, ...]: ");
-//        String input = scanner.nextLine().trim();
-//
-//        String cleanedInput = input.replaceAll("[\\[\\] ]", "");
-//        String[] elements = cleanedInput.split(",");
-//
-//        int employeeCount = 0;
-//        for (String element : elements) {
-//            if (element.equals("1")) {
-//                employeeCount++;
-//            }
-//        }
-//
-//        System.out.println("Количество сотрудников: " + employeeCount);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите массив в формате [1, 0, 1, 1, ...]: ");
+        String input = scanner.nextLine().trim();
+
+        String cleanedInput = input.replaceAll("[\\[\\] ]", "");
+        String[] elements = cleanedInput.split(",");
+
+        int employeeCount = 0;
+        for (String element : elements) {
+            if (element.equals("1")) {
+                employeeCount++;
+            }
+        }
+
+        System.out.println("Количество сотрудников: " + employeeCount);
+        
 //        Vowels vowels = new Vowels();
 //        vowels.removeVowels();
-        BestSumFinder bsf = new BestSumFinder();
-        bsf.run();
+//
+//        BestSumFinder bsf = new BestSumFinder();
+//        bsf.run();
     }
 }
